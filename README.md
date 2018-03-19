ee21e3a683756a345476953a85976c4573dede03
xxfoofyxx <37008464+xxfoofyxx@users.noreply.github.com>
2018-03-19 13:06:48 -0400

    Update.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# fCrypt
+# fCrypt by xxfoofyxx
 File encryption software written in Batch.
+
+To install the software, download and open fCrypt_setup.bat
+To uninstall the software, nevigate to the same directory as the setup and type
+`fCrypt_setup uninstall`
--- installer.bat
+++ installer.bat
@@ -18,18 +18,16 @@ echo Setup is now installing fCrypt on your computer...
 echo --------------------------------------------------
 echo.
 echo Downloading latest version...
-call dl https://raw.githubusercontent.com/xxfoofyxx/fCrypt/master/fCrypt.bat fCrypt.bat
+call dl https://raw.githubusercontent.com/xxfoofyxx/fCrypt/master/source/fCrypt.bat fCrypt.bat 2 >nul
 mkdir "C:\Program Files\fCrypt"
-move fCrypt.bat "C:\Program Files\fCrypt.bat"
-echo Downloading encryption utility...
-call dl https://raw.githubusercontent.com/xxfoofyxx/fCrypt/master/aes.exe aes.exe
-move aes.exe "C:\Program Files\fCrypt\aes.exe"
+move fCrypt.bat "C:\Program Files\fCrypt\fCrypt.bat" >nul
+call dl https://raw.githubusercontent.com/xxfoofyxx/fCrypt/master/source/aes.exe aes.exe 2 >nul
+move aes.exe "C:\Program Files\fCrypt\aes.exe" >nul
 cd "C:\Program Files\fCrypt"
-echo true>verify
-echo Setup is now complete. Would you like to open fCrypt now?
-choice
-if %ERRORLEVEL%==1 call fCrypt -install
+::20 zeros because the aes utility requires at least 16 bytes (20 0s = 20 bytes)
+echo 00000000000000000000>verify
 cls
+echo Setup is now complete.
 echo Press any key to exit Setup...
 pause >nul
 exit /b
@@ -42,6 +40,7 @@ echo Are you COMPLETELY sure you want to do this? This is your last chance!
 choice
 if %ERRORLEVEL%==2 echo Uninstall canceled. & exit /b
 echo Ok, uninstalling now.
-rmdir "C:\Program Files\fCrypt"
+del "C:\Program Files\fCrypt\*" /q /y
+rmdir "C:\Program Files\fCrypt" /s /q
 echo Uninstall completed.
 exit /b
